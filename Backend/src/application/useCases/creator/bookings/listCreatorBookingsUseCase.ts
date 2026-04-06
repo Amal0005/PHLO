214670871d718571287aa7620048a08b93c5db7e
Amal0005 <amalachu441345@gmail.com>
2026-04-06 23:16:42 +0530

    feat: implement ListCreatorBookingsUseCase to retrieve and map creator bookings

--- Backend/src/application/useCases/creator/bookings/listCreatorBookingsUseCase.ts
+++ Backend/src/application/useCases/creator/bookings/listCreatorBookingsUseCase.ts
@@ -1,5 +1,6 @@
 import { BookingMapper } from "@/application/mapper/user/bookingMapper";
 import type { BookingResponseDTO } from "@/domain/dto/booking/bookingResponseDto";
+import type { BookingEntity } from "@/domain/entities/bookingEntity";
 import type { IListCreatorBookingsUseCase } from "@/domain/interfaces/creator/bookings/IListCreatorBookingsUseCase";
 import type { IBookingRepository } from "@/domain/interfaces/repository/IBookingRepository";
 
@@ -8,7 +9,7 @@ export class ListCreatorBookingsUseCase implements IListCreatorBookingsUseCase {
         private _bookingRepo: IBookingRepository
     ) {}
     async listBookings(creatorId: string): Promise<BookingResponseDTO[]> {
-        const bookings = await this._bookingRepo.findByCreatorId(creatorId)
-        return bookings.map(item => BookingMapper.toDto(item))
+        const result = await this._bookingRepo.findByCreatorId(creatorId, 1, 50)
+        return result.bookings.map((item: BookingEntity) => BookingMapper.toDto(item))
     }
-} 
+}
